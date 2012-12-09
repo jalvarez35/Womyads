3d96745cb42e043fa27c775d7014b551f931687e
Juan Gabriel Alvarez <juangabo.0228@gmail.com>
2012-12-09 15:03:25 -0500

    creacion controladores

--- Womyads/Controllers/Contacto.cs
+++ Womyads/Controllers/Contacto.cs
@@ -1,1 +1,19 @@
-﻿ 
+﻿using Womyads.Models;
+using System.Web.Mvc;
+
+namespace Womyads.Controllers
+{
+    public class ContactoController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(Contacto elContacto)
+        {
+            return View();
+        }
+    }
+}
--- Womyads/Controllers/Registro.cs
+++ Womyads/Controllers/Registro.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using Womyads.Models;
+using System.Web.Mvc;
+
+namespace Womyads.Controllers
+{
+    public class RegistroController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(Registro elRegistro)
+        {
+            return View();
+        }
+    }
+}
