7f7ec33d74dc6c6804ac9abde7459c020583b470
Juan Gabriel Alvarez <juangabo.0228@gmail.com>
2012-12-09 22:52:57 -0500

    creación de gráficas

---
+++ Womyads/Controllers/Tablero.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Womyads.Controllers
+{
+    public class TableroController : Controller
+    {
+        public ActionResult Show()
+        {
+            return View();
+        }
+    }
+}
